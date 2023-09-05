b1aa0a63c6dd1e6906a1b98f33734301e4062c0c
Bobbie Goede <bobbiegoede@gmail.com>
2023-09-05 15:43:55 +0200

    chore: setup v8 starter

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
+<script lang="ts" setup>
+import LangSwitcher from "@/components/LangSwitcher.vue";
+const { locales } = useI18n();
+const localeCodes = computed(() => locales.value.map((x) => (typeof x === "string" ? x : x.code)));
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+	<div>
+		<LangSwitcher />
+		<NuxtPage></NuxtPage>
+	</div>
 </template>
+
+<style>
+:root {
+	font-family: sans-serif;
+}
+
+</style>
